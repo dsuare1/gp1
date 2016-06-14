eeaf0cff2c043365f6c2013f4c17879f0a874370
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-06-13 19:52:44 -0500

    temporary branch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
 var clickCounter = 0;
+
+
+// Dropdown menu options for Create Game page
+// $("#first-choice").change(function() {
+//    $("#second-choice").load("textdata/" + $(this).val() + ".txt");
+// });
