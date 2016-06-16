1c279ba429cf1091e00003e0f1b9064447340f5c
dsuare1 <suarez.derrick@gmail.com>
2016-06-15 20:51:33 -0500

    added firebase population logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,44 +2,6 @@
 // CLICK LISTENERS
 ///////////////////////////////////////////////////////////
 
-///////// 'findGame.html' SPORTS BUTTONS /////////
-
-// $(".sport-search").on("click", function() {
-
-//     locSearch = $(this).attr("id");
-
-//     if ($(this).attr("id") == "basketball") {
-//         $(".visible").removeClass("visible").addClass("hidden");
-//         $("#basketball-map")
-//             .removeClass("hidden")
-//             .addClass("visible")
-//             .fadeIn(500);
-//         $("#wind-warning").addClass("hidden");
-//         $("#temp-warning").removeClass("hidden");
-//     };
-
-//     if ($(this).attr("id") == "soccer") {
-//         $(".visible").removeClass("visible").addClass("hidden");
-//         $("#soccer-map")
-//             .removeClass("hidden")
-//             .addClass("visible")
-//             .fadeIn(500);
-//         $("#wind-warning").addClass("hidden");
-//         $("#wind-warning").addClass("hidden");
-//     };
-
-//     if ($(this).attr("id") == "disc-golf") {
-//         $(".visible").removeClass("visible").addClass("hidden");
-//         $("#disc-golf-map")
-//             .removeClass("hidden")
-//             .addClass("visible")
-//             .fadeIn(500);
-//         $("#temp-warning").addClass("hidden");
-//         $("#wind-warning").removeClass("hidden");
-//     };
-
-// });
-
 
 ///////// 'createGame.html' FORM /////////
 
@@ -81,12 +43,83 @@ $(".dropdown-menu li a").on("click", function() {
     // pulsating text effect
     $(function() {
         var p = $(".pulsate-text");
-        for(var i=0; i<3; i++) {
-        p.animate({opacity: 0.2}, 1000, 'linear').animate({opacity: 1}, 1000, 'linear');
-    }
+        for (var i = 0; i < 3; i++) {
+            p.animate({ opacity: 0.2 }, 1000, 'linear').animate({ opacity: 1 }, 1000, 'linear');
+        }
 
     });
 });
+
+////////////////////////////////////
+// VALIDATION FUNCTIONS
+////////////////////////////////////
+function checkUserName(pickupUserName) {
+    // regular expression to match required user name format
+    re = /[a-z]\w+/gi;
+
+    if (pickupUserName !== '' && !pickupUserName.match(re)) {
+        vex.dialog.alert("Please enter a valid destination");
+        return false;
+    }
+    return true;
+};
+
+function checkGameName(pickupGameName) {
+    // regular expression to match required game name format
+    re = /[a-z]\w+/gi;
+
+    if (pickupGameName !== '' && !pickupGameName.match(re)) {
+        vex.dialog.alert("Please enter a valid destination");
+        return false;
+    }
+    return true;
+};
+
+function checkLocation(pickupLocation) {
+
+    if (pickupLocation !== '') {
+        vex.dialog.alert("Please enter a valid location (either the place name or the full address)");
+        return false;
+    }
+    return true;
+};
+
+function checkDate(pickupDate) {
+    // regular expression to match required date format
+    re = /^([0-1]?[0-9]|2[0-3])(:[0-5][0-9])?$/;
+
+    if (pickupDate !== '' && !pickupDate.match(re)) {
+        vex.dialog.alert("Please enter a valide date");
+        return false;
+    }
+    return true;
+};
+
+function checkTime(pickupTime) {
+    // regular expression to match required date format
+    re = /^([0-1]?[0-9]|2[0-3])(:[0-5][0-9])?$/;
+
+    if (pickupTime !== '' && !pickupTime.match(re)) {
+        vex.dialog.alert("Please enter a valid time")
+    }
+}
+
+///////// 'findGame.html' CHILDREN ADDED FROM FIREBASE /////////
+
+$("#individual-game-holder").on("click", function() {
+    var location = $("#game-location").text();
+
+    console.log(location);
+
+    location = encodeURIComponent(location.trim());
+
+    console.log(location);
+
+    $(".find-game-map").removeClass("visible").addClass("hidden");
+
+    $("#map-holder").html("<iframe class='visible' width='100%' height='440' frameborder='0' src='https://www.google.com/maps/embed/v1/search?q=" + location + "&amp;center=30.2672%2C-97.7431&amp;zoom=10&amp;key=AIzaSyDchdsjpvalXui_QTAFtm9Hb1Ka67X5s1k'></iframe>");
+});
+
 
 // DATE-TIME PICKER
 $("#datetimepicker1").on("click", function() {
@@ -163,7 +196,12 @@ $.ajax({ url: queryURL, method: 'GET' }).done(function(response) {
 var pickupData = new Firebase("https://pkupgames.firebaseio.com/");
 
 // 2. Button for adding pickupGame
-$("#addPickupGame").on("click", function(e){
+$("#addPickupGame").on("click", function(e) {
+
+    if (($("#user-name-input").val() == "") || ($("#game-name-input").val() == "") || ($("#location-input").val() == "") || ($("#date-input").val()) == "" || ($("#time-input").val() == "" )) {
+            vex.dialog.alert("Please enter the necessary information into the form");
+            return false;
+        }
 
     // prevents reloading of page
     e.preventDefault();
@@ -175,16 +213,18 @@ $("#addPickupGame").on("click", function(e){
     var pickupLocation = $("#location-input").val().trim();
     var pickupDate = $("#datetimepicker1").val().trim();
     var pickupTime = $("#datetimepicker3").val().trim();
-    
+
     // Creates local "temporary" object for holding pickup data
-    var newPickup = {
-        type: pickupGameType,
-        userName: pickupUserName,
-        gameName: pickupGameName,
-        location: pickupLocation,
-        date: pickupDate,
-        time: pickupTime
-    }
+
+    if (checkUserName(pickupUserName) && checkGameName(pickupGameName) && checkLocation(pickupLocation) && checkDate(pickupDate) && checkTime(pickupTime)) {
+        var newPickup = {
+            type: pickupGameType,
+            userName: pickupUserName,
+            gameName: pickupGameName,
+            location: pickupLocation,
+            date: pickupDate,
+            time: pickupTime
+    }   
 
     // Uploads pickup data to the database
     pickupData.push(newPickup);
@@ -208,7 +248,7 @@ $("#addPickupGame").on("click", function(e){
 });
 
 
-pickupData.on("child_added", function(childSnapshot, prevChildKey){
+pickupData.on("child_added", function(childSnapshot, prevChildKey) {
 
     console.log(childSnapshot.val());
 
@@ -220,7 +260,7 @@ pickupData.on("child_added", function(childSnapshot, prevChildKey){
     var pickupDate = $("#datetimepicker1").val().trim();
     var pickupTime = $("#datetimepicker3").val().trim();
 
-    // $(".active-game").append("<p")
+    $(".active-games").append("<h4>Game type:</h4><p id='game-type'>" + pickupGameType + "</p><br><h4>Created by:</h4><p id='created-by'>" + pickupUserName + "</p><br><h4>Game name:</h4><p id='game-name'>" + pickupGameName + "</p><br><h4>Location:</h4><p id='game-location'>" + pickupLocation + "</p><br><h4>Date:</h4><p id='game-date'>" + pickupDate + "</p><br><h4>Time</h4><p id='game-time'>" + pickupTime + "</p>");
 
     // // Prettify the pickup Time
     // var pickupTimePretty = moment.unix(pickupTime).format("MM/DD/YY");
